a0446811b7454b3f58ff74fbe65a42bb3960f761
manuel.Lara <manuel.lara@peoplemtech.com>
2025-11-25 17:20:15 +0000

    Update App Text 2

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -30,7 +30,7 @@ function App() {
       </p>
       <p>PULL REQUEST TEST</p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
