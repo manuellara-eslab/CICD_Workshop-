1c2b33360eb0f87f699c94b00b22a3a94235c051
manuel.Lara <manuel.lara@peoplemtech.com>
2025-11-25 18:28:35 +0000

    Implemented CI pipeline

--- app-cdk/lib/pipeline-cdk-stack.ts
+++ app-cdk/lib/pipeline-cdk-stack.ts
@@ -1,10 +1,14 @@
 import { Stack, StackProps, CfnOutput } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { aws_codeconnections as codeconnections } from "aws-cdk-lib";
+import * as codepipeline from "aws-cdk-lib/aws-codepipeline";
+import * as codebuild from "aws-cdk-lib/aws-codebuild";
+import * as codepipeline_actions from "aws-cdk-lib/aws-codepipeline-actions";
 
 export class PipelineCdkStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
+
     const SourceConnection = new codeconnections.CfnConnection(
       this,
       "CICD_Workshop_Connection",
@@ -13,6 +17,53 @@ export class PipelineCdkStack extends Stack {
         providerType: "GitHub",
       }
     );
+
+    const pipeline = new codepipeline.Pipeline(this, "Pipeline", {
+      pipelineName: "CICD_Pipeline",
+      crossAccountKeys: false,
+      pipelineType: codepipeline.PipelineType.V2,
+      executionMode: codepipeline.ExecutionMode.QUEUED,
+    });
+
+    const codeBuild = new codebuild.PipelineProject(this, "CodeBuild", {
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_7_0,
+        privileged: true,
+        computeType: codebuild.ComputeType.LARGE,
+      },
+      buildSpec: codebuild.BuildSpec.fromSourceFilename("buildspec_test.yml"),
+    });
+
+    const sourceOutput = new codepipeline.Artifact();
+    const unitTestOutput = new codepipeline.Artifact();
+
+    pipeline.addStage({
+      stageName: "Source",
+      actions: [
+        new codepipeline_actions.CodeStarConnectionsSourceAction({
+          actionName: "GitHub",
+          owner: "{{manuellara-eslab}}",
+          repo: "CICD_Workshop",
+          output: sourceOutput,
+          branch: "main",
+          connectionArn:
+            "{{arn:aws:codeconnections:us-east-1:700298696536:connection/274f0b9f-9558-4309-bd84-1965b3fbe877}}",
+        }),
+      ],
+    });
+
+    pipeline.addStage({
+      stageName: "Code-Quality-Testing",
+      actions: [
+        new codepipeline_actions.CodeBuildAction({
+          actionName: "Unit-Test",
+          project: codeBuild,
+          input: sourceOutput,
+          outputs: [unitTestOutput],
+        }),
+      ],
+    });
+
     new CfnOutput(this, "SourceConnectionArn", {
       value: SourceConnection.attrConnectionArn,
     });
